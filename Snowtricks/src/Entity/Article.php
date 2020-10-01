26f938b1c4373ba3b0300a1c933f6a9629fd7257
Anne Marchasson <annemarchasson@yahoo.fr>
2020-10-01 16:15:04 +0200

    add table category

--- Snowtricks/src/Entity/Article.php
+++ Snowtricks/src/Entity/Article.php
@@ -43,6 +43,12 @@ class Article
      */
     private $createdAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -81,6 +87,18 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
