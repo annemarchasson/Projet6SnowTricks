fb6f63cab285aba90d8d2b2ce1a93bcde1d499da
Anne Marchasson <annemarchasson@yahoo.fr>
2020-10-01 16:56:29 +0200

    add table image and table video and bundles upload vich for upload image figure

---
+++ Snowtricks/src/Entity/Image.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=ImageRepository::class)
+ * @Vich\Uploadable
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
+
+
+
+    /**
+     * @var File|null
+     * @Vich\UploadableField(mapping="figures", fileNameProperty="filename")
+     */
+    private $imageFile;
+
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Article::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $articles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string|null $filename
+     * @return Image
+     */
+    public function setFilename(?string $filename): Image
+    {
+        $this->filename = $filename;
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param File|null $imageFile
+     * @return Image
+     */
+    public function setImageFile(?File $imageFile): Image
+    {
+        $this->imageFile = $imageFile;
+        return $this;
+    }
+    public function getArticles(): ?Article
+    {
+        return $this->articles;
+    }
+
+    public function setArticles(?Article $articles): self
+    {
+        $this->articles = $articles;
+
+        return $this;
+    }
+
+
+}
---
+++ Snowtricks/src/Entity/Video.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VideoRepository::class)
+ */
+class Video
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $link;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Article::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $articles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getArticles(): ?Article
+    {
+        return $this->articles;
+    }
+
+    public function setArticles(?Article $articles): self
+    {
+        $this->articles = $articles;
+
+        return $this;
+    }
+}
